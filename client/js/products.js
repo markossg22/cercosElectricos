8412a20927cd403b8e78d9c7832ac97e243e0479
markossg <markoscortezg@gmail.com>
2024-11-20 10:56:33 -0300

    carritoCompras

---
+++ client/js/products.js
@@ -0,0 +1,23 @@
+const productos = [
+    {
+        id: 1,
+        productName: "objeto1",
+        price: 10,
+        quanty: 1,
+        img : "sources/manzanita.png"
+    },
+    {
+        id: 2,
+        productName: "objeto2",
+        price: 15,
+        quanty: 1,
+        img : "sources/manzanita.png"
+    },
+    {
+        id: 3,
+        productName: "objeto3",
+        price: 20,
+        quanty: 1,
+        img : "sources/manzanita.png"
+    }
+];
